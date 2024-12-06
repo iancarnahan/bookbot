a18088412b7babfa4a671c6db4856e6d9a780323
iancarnahan <ian.carnahan@gmail.com>
2024-12-06 10:30:41 +0100

    word count function completed

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
-    print(file_contents)  # This will print the entire book!
+    total_words = wordcount(file_contents)
+    return total_words
+
+def wordcount(file_contents):
+    words = file_contents.split()
+    word_count = len(words)
+    return word_count
+
+word_count_result = main()
+print(word_count_result)
 
 main()
