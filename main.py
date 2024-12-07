a7d58c83634144345cdc54fe97afb1ab468512af
iancarnahan <ian.carnahan@gmail.com>
2024-12-07 09:12:08 +0100

    Character Count Complete

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ def main():
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
         text = f.read()
+    
     total_words = wordcount(text)
-    return total_words
+    char_count = character_count(text)
+
+    return total_words, char_count
 
 def wordcount(text):
     words = text.split()
     return len(words)
 
-word_count_result = main()
-print(word_count_result)
+def character_count(text):
+    character_count = {}
+    for char in text:
+        char = char.lower()
+        if char in character_count:
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+    return character_count
 
-main()
+word_count_result, char_count_result = main()
+print(word_count_result)
+print(char_count_result)
